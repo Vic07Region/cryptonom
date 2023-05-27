76f07eaefefd6aa08b6526b506448e40d96519bc
blacklotus <lotus-xx@ya.ru>
2023-05-28 00:57:22 +0500

    reload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tw-elements/dist/plugin.cjs"),
+  ],
+};
